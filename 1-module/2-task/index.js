8045207e98dc8b6bfadd381d1ab7fc9c5466d299
Katerina Lyakhova <kittylynx111@gmail.com>
2025-05-25 18:35:08 +0300

    Все задания к 1 уроку

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,8 +10,14 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  return (
+    typeof name === 'string' &&
+    name.trim() !== '' &&
+    !name.includes(' ') &&
+    name.length >= 4
+  );
 }
+
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
