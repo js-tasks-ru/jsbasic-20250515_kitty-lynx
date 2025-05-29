434784596fe9d207badf6ff9e0e5595af68b2977
Katerina Lyakhova <kittylynx111@gmail.com>
2025-05-29 12:15:20 +0300

    Убрала из решения объект sumSalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -7,13 +7,3 @@ function sumSalary(salaries) {
   }
   return sum;
 }
-
-let salaries = {
-  John: 1000,
-  Ann: 1600,
-  Pete: 1300,
-  month: 'December',
-  currency: 'USD',
-  isPayed: false
-}
-
