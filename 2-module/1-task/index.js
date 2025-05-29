b91328a15bedecb2adf27faa93b978878c9ba442
Katerina Lyakhova <kittylynx111@gmail.com>
2025-05-29 12:09:27 +0300

    решение задачи 1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,19 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number' && key !== 'isPayed') {
+      sum += salaries[key];
+    }
+  }
+  return sum;
 }
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
