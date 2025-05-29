54bd1df64f21b2422ceb19b56b9d7d3be2a1a31f
Katerina Lyakhova <kittylynx111@gmail.com>
2025-05-29 12:36:34 +0300

    и еще одна попытка написать правильно

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
 function sumSalaries(salaries) {
   let sum = 0;
   for (let key in salaries) {
