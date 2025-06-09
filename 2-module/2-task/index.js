10f3ee39426b3f52539dcaddcfa318296018d369
Katerina Lyakhova <kittylynx111@gmail.com>
2025-06-09 19:41:46 +0300

    решение задач 2.2 и 3.1

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,14 +1,7 @@
-function isEmpty(obj) 
+function isEmpty(obj){
   for (let key in obj) {
     return false;
   }
   return true;
 }
-
 let schedule = {};
-
-alert( isEmpty(schedule) ); // true
-
-schedule["8:30"] = "подъём";
-
-alert( isEmpty(schedule) ); // false
